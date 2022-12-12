4ad6207de1390d484ed6c65b6bc4dc673945fa16
Robert <robertjach74@gmail.com>
2022-12-12 23:04:29 +0100

    - API client authorization method changed from URL parameter to header x-api-key parameter using RestTemplate - code of the SpooncularClient class has been optimized for encapsulation

--- diet/src/main/java/com/simplediet/app/webclient/dto/client/SpooncularClient.java
+++ diet/src/main/java/com/simplediet/app/webclient/dto/client/SpooncularClient.java
@@ -1,10 +1,12 @@
 package com.simplediet.app.webclient.dto.client;
 
 import com.simplediet.app.model.entity.spoonacularingredients.IngredientBasicObject;
-import com.simplediet.app.model.entity.spoonacularproduct.*;
+import com.simplediet.app.model.entity.spoonacularproduct.BasicProductData;
+import com.simplediet.app.model.entity.spoonacularproduct.InformationProduct;
+import com.simplediet.app.model.entity.spoonacularproduct.Product;
 import com.simplediet.app.webclient.dto.spooncularDTO.SpoonacularDTO;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
 
@@ -16,19 +18,14 @@ public class SpooncularClient {
     private static final String SPOONCULAR_INGREDIENTS_URL = "https://api.spoonacular.com/food/ingredients";
     private static final String API_KEY = "2d6514dae01e4847b6868ee84b9cf868";
 
-    private final RestTemplate restTemplate = new RestTemplate();
     private final SpoonacularDTO spoonacularDTO = new SpoonacularDTO();
 
     public InformationProduct getProductInformation(long id) {
-        return spoonacularDTO.convert(
-                callGetMethod(SPOONCULAR_PRODUCT_URL, "/{id}?apiKey={apiKey}", String.class, id, API_KEY),
-                InformationProduct.class);
+        return spoonacularDTO.convert(callGetMethod(SPOONCULAR_PRODUCT_URL, "/{id}", id), InformationProduct.class);
     }
 
     public InformationProduct getProductInformationWithUpcCode(String upc) {
-        return spoonacularDTO.convert(
-                callGetMethod(SPOONCULAR_PRODUCT_URL, "/upc/{upc}?apiKey={apiKey}", String.class, upc, API_KEY),
-                InformationProduct.class);
+        return spoonacularDTO.convert(callGetMethod(SPOONCULAR_PRODUCT_URL, "/upc/{upc}", upc), InformationProduct.class);
     }
 
     public Product getProduct(String query, boolean addProductInformation, int number) {
@@ -36,32 +33,26 @@ public class SpooncularClient {
         if (addProductInformation) {
             product.setProducts(Collections.singletonList(BasicProductData.class));
             return spoonacularDTO.convert(callGetMethod(SPOONCULAR_PRODUCT_URL,
-                    "/search?query={query}" +
-                            "&addProductInformation={addProductInformation}" +
-                            "&number={number}" +
-                            "&apiKey={apiKey}",
-                    String.class, query, true, number, API_KEY), Product.class);
+                    "/search?query={query}&addProductInformation={addProductInformation}&number={number}",
+                    query, true, number), Product.class);
         } else
             product.setProducts(Collections.singletonList(InformationProduct.class));
         return spoonacularDTO.convert(callGetMethod(SPOONCULAR_PRODUCT_URL,
-                "/search?query={query}" +
-                        "&addProductInformation={addProductInformation}" +
-                        "&number={number}" +
-                        "&apiKey={apiKey}",
-                String.class, query, false, number, API_KEY), Product.class);
+                "/search?query={query}&addProductInformation={addProductInformation}&number={number}",
+                query, false, number), Product.class);
     }
 
     public IngredientBasicObject getIngredientData(String query, boolean addChildren, boolean metaInformation, String sortDirection, int number) {
-        return spoonacularDTO.convert(callGetMethod(SPOONCULAR_INGREDIENTS_URL, "/search?query={query}" +
-                "&addChildren={addChildren}" +
-                "&metaInformation={metaInformation}" +
-                "&sortDirection={sortDirection}" +
-                "&number={number}" +
-                "&apiKey={apiKey}", String.class, query, addChildren, metaInformation, sortDirection, number, API_KEY),
-                IngredientBasicObject.class);
+        return spoonacularDTO.convert(callGetMethod(SPOONCULAR_INGREDIENTS_URL,
+                "/search?query={query}&addChildren={addChildren}&metaInformation={metaInformation}&sortDirection={sortDirection}&number={number}",
+                query, addChildren, metaInformation, sortDirection, number), IngredientBasicObject.class);
     }
 
-    private <T> T callGetMethod(String basicURL, String categoryURL, Class<T> responseType, Object... objects) {
-        return restTemplate.getForObject(basicURL + categoryURL, responseType, objects);
+    private String callGetMethod(String basicUri, String categoryUrl, Object... objects) {
+        return new RestTemplateBuilder()
+                .rootUri(basicUri)
+                .defaultHeader("x-api-key", API_KEY)
+                .build()
+                .getForObject(categoryUrl, String.class, objects);
     }
 }
